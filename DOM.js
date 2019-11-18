949dfb888c559c0fee48399389144083d7c8bfbc
SofiaMijares <is715699@iteso.mx>
2019-11-18 16:58:38 -0600

    Update ventanas

---
+++ DOM.js
@@ -0,0 +1,7 @@
+var e = document.getElementById('icon');
+e.onmouseover = function() {
+  document.getElementById('info').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('info').style.display = 'none';
+}
