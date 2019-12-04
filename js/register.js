51a174ca5a5f06afa9c3cf539ef975e0a600be8c
JulioGiovanni <44816899+JulioGiovanni@users.noreply.github.com>
2019-12-04 13:46:10 -0600

    new update

--- js/register.js
+++ js/register.js
@@ -1,11 +1,15 @@
-let req = new XMLHttpRequest();
-req.open('GET', 'http://www.mozilla.org/', true);
-req.onreadystatechange = function (aEvt) {
-  if (req.readyState == 4) {
-     if(req.status == 200)
-      dump(req.responseText);
-     else
-      dump("Error loading page\n");
-  }
-};
-req.send(null); 
+function Registro(){
+    let req = new XMLHttpRequest();
+    req.open('POST', 'http://www.mozilla.org/', true);
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState == 4) {
+         if(req.status == 200)
+          dump(req.responseText);
+         else
+          dump("Error loading page\n");
+      }
+    };
+    req.send(null); 
+}
+
+
